d04760c20d742cd10b3536072e0e8199fabfcccb
Marc Galvez <marc.galvez83@gmail.com>
2023-08-01 01:56:16 -0400

    Near completion need to add try again and score.

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 CallAllFunctions()
 
 const Rock = {
+    Name: "Rock",
     Weakness: "Paper",
     Strength: "Scissors",
     Draw:      "Rock"
 
 };
 const Paper = {
+    Name: "Paper",
     Weakness: "Scissors",
     Strength: "Rock",
     Draw:      "Paper"
 };
 const Scissors = {
+    Name: "Scissors",
     Weakness: "Rock",
     Strength: "Paper",
     Draw:      "Scissors"
@@ -34,36 +37,19 @@ function CallAllFunctions() {
     btnScissors.addEventListener("click",  function() {
        ChangeDisplay(3);
     });
+
+    
 }
 function ChangeDisplay(choice) {
     document.getElementById("starting-page").classList.add('d-none');
     document.getElementById("versus-page").classList.remove('d-none');
+    document.getElementById("winner").classList.remove('d-none');
 
+    var randomChoice = Math.floor(Math.random() * 3) + 1;
+    var imgUser = document.getElementById("user");
+    var imgComputer = document.getElementById("computer");
 
-
-
-
-    DisplayMove(choice);
-    SelectWinner();
-}
-
-function SelectWinner() {
-    winner = ""
-    if (userChoice.Strength === ComputerChoiceOBJ.Weakness) {
-        winner = "user";
-    } else if (userChoice.Weakness === ComputerChoiceOBJ.Strength) {
-        winner = "computer";
-    }
-    else {
-        winner = "draw";
-    }
-}
-
-function DisplayMove(choice) {
-    var randomChoice = Math.floor(Math.random() * 3) + 1
-    var imgUser = document.getElementById("user")
-    var imgComputer = document.getElementById("computer")
-    console.log(randomChoice);
+    var winnerDisplay = "";
     switch(choice) {
         case 1:
             imgUser.src = 'img/rock.png'
@@ -80,6 +66,7 @@ function DisplayMove(choice) {
         default:
             console.log("Buttons have stopped working.");
     }
+
     switch(randomChoice) {
         case 1:
             imgComputer.src = 'img/rock.png'
@@ -96,4 +83,46 @@ function DisplayMove(choice) {
         default:
             console.log("Random choice has stopped working.");
     }
+
+
+    winner = SelectWinner();
+    winnerDisplay += '<h1 class="text-center text-light mt-5">';
+    switch(winner) {
+        case "user":
+            winnerDisplay += 'Amazing you win!';
+            break;
+        case "computer":
+            winnerDisplay += "You lost, Try again!";
+            break;
+        case "draw":
+            winnerDisplay += "It's a draw, Try again!";
+            break;
+        default:
+            window.alert("The application has failed!");
+    }
+    winnerDisplay += '</h1>';
+    document.getElementById('winner').innerHTML = winnerDisplay;
 }
+
+function SelectWinner() {
+    var winner = ""
+    if (userChoice.Strength === ComputerChoiceOBJ.Name) {
+        winner = "user";
+        console.log("1st");
+    }
+    else if (userChoice.Weakness === ComputerChoiceOBJ.Name) {
+        winner = "computer";
+        console.log("2nd")
+    }
+    else if (userChoice.Draw  === ComputerChoiceOBJ.Name)  {
+        winner = "draw";
+    }
+    else {
+        console.log("Error")
+    }
+
+    
+    return winner;
+
+}
+
